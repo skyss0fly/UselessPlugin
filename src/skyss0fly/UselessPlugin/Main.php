555f4fcba754e5eb119b7fa570293956aced47f8
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2025-01-05 08:51:02 +1000

    Create Main.php

---
+++ src/skyss0fly/UselessPlugin/Main.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace skyss0fly\UselessPlugin;
+
+use pocketmine\plugin\PluginBase;
+
+Class Main extends PluginBase {
+
+public function onLoad(): void {
+$this->getLogger()->info("Hello Jason i am watching you review plugins right now");
+}
+  
+  }
+
+  ?>
